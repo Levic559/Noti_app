171edd0b1591664da2a2f366f55f79d00528fc50
ChenWarren <hchen273@my.bcit.ca>
2021-05-13 10:59:01 -0700

    solved problem

---
+++ comps/Video/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+// import ReactPlayer from 'react-player/lazy'
+
+const VideoCont = styled.div`
+    max-width: 1920px;
+    max-height: 1080px;
+    margin: 30px;
+`
+
+const Video =({
+    // VideoSrc={video},
+    autoPlay='autoplay'
+})=>
+{
+    return<VideoCont>
+        <ReactPlayer 
+        url='/promote.mp4'
+        width='70%'
+        height='70%' 
+        controls 
+        muted/>
+    </VideoCont>
+}
+
+// export default Video
