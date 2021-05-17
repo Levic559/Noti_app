b59e90a461862259f434130eec637b0351745011
AdrianTejada <atejada@my.bcit.ca>
2021-05-17 14:00:26 -0700

    finished features

---
+++ comps/Feature/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ScrenCont = styled.div`
+    width: 342px;
+    height: 742px;
+    background-image: url(${props=>props.imgUrl});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    border: solid grey 1px;
+`
+
+const Screen =({
+    imgUrl='schedule.png'
+})=>
+{
+    return<ScrenCont imgUrl={imgUrl}/>
+}
+
+export default Screen
--- pages/features.js
+++ pages/features.js
@@ -1,11 +1,5 @@
-import Logo from '../comps/Logo'
-import Button from '../comps/Button'
-import Avatar from '../comps/Avatar'
-import Screen from '../comps/Screen'
-import VideoUI from '../comps/VideoUI'
+import Feature from '../comps/Feature'
 import PageText from '../comps/PageText'
-import BigText from '../comps/BigText'
-import MenuLi from '../comps/MenuLi'
 import Header from '../comps/Header'
 import styled from 'styled-components';
 
@@ -16,16 +10,89 @@ const Cont = styled.div `
   flex-direction: column;
 `
 
+const WhiteCont = styled.div`
+display:flex;
+justify-content:space-between;
+align-items:center;
+width: 100vw;
+height: 878px;
+`
+const BlackCont = styled.div`
+display:flex;
+justify-content:space-between;
+align-items:center;
+background-color: rgb(174, 159, 175, 0.2);
+border-top: 2px solid #AE9FAF;
+border-bottom: 2px solid #AE9FAF;
+width: 100vw;
+height: 878px;
+`
+
+const Left = styled.div`
+  position:relative;
+  left:148px;
+`
+
+const Right = styled.div`
+position:relative;
+right:148px;
+`
+
 export default function Features() {
   return <Cont>
 
 
       <Header/>
+      <WhiteCont>
+        <Left>
+          <PageText 
+            titleText="Dashboard"
+            bodyText="Make your choice of essential events or to-do lists for you to pin to the dashboard."
+          />
+        </Left>
 
+        <Right>
+          <Feature imgUrl='dashboard.png'/>
+        </Right>
+      </WhiteCont>
 
-      <PageText />
+      <BlackCont>
+        <Left>
+          <Feature imgUrl="schedule.png"/>
+        </Left>
 
-      <Screen imgUrl='note.png'/>
-      <Screen imgUrl='todo.png'/>
+        <Right>
+          <PageText
+            titleText="Schedule"
+            bodyText="Add events to dates, as well as add lists, notes and reminders for your convenience. "
+          />
+        </Right>
+      </BlackCont>
+
+      <WhiteCont>
+        <Left>
+          <PageText 
+            titleText="Notes"
+            bodyText="Jot down on-the-go thoughts or take time for more detailed entries."
+          />
+        </Left>
+
+        <Right>
+          <Feature imgUrl='note.png'/>
+        </Right>
+      </WhiteCont>
+
+      <BlackCont>
+        <Left>
+          <Feature imgUrl="todo.png"/>
+        </Left>
+
+        <Right>
+          <PageText
+            titleText="To-Do Lists"
+            bodyText="An essential for any productivity app.  "
+          />
+        </Right>
+      </BlackCont>
   </Cont>
 }
